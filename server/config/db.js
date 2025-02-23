7bc1992b02d1689d1b4aba4f4949a594059140b6
Umad Akram <152366259+AkramUmad572@users.noreply.github.com>
2025-02-22 20:30:01 -0500

    API

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,5 @@
-module.exports={"URI":"mongodb+srv://{InsertUsername + Password}"}
+module.exports={"URI":"mongodb+srv://umadakram:Rarror1379%40@cluster0.gxyz5.mongodb.net/"}
+
+
+//mongodb+srv://umadakram:Rarror1379%40@cluster0.gxyz5.mongodb.net/
+ //await mongoose.connect('mongodb+srv://ahmedsheikh:Test123@cluster0.0f3pz.mongodb.net/');
