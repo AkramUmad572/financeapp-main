91c7ac10bb6b47a7e76e1af513e7487b2986f988
Umad Akram <152366259+AkramUmad572@users.noreply.github.com>
2025-02-22 20:52:35 -0500

    server/config/db.js removed

--- server/config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports={"URI":"mongodb+srv://umadakram:Rarror1379%40@cluster0.gxyz5.mongodb.net/"}
-
-
-//mongodb+srv://umadakram:Rarror1379%40@cluster0.gxyz5.mongodb.net/
- //await mongoose.connect('mongodb+srv://ahmedsheikh:Test123@cluster0.0f3pz.mongodb.net/');
